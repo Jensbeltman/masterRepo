14951d2637a88d5ffae008b5d8e1ae2ec4540621
jensbeltman <jens@beltman.dk>
2020-10-15 09:59:41 +0200

    Added logging and vizualiation utility functions

--- lib/GA/include/ga/utility/vizualization.hpp
+++ lib/GA/include/ga/utility/vizualization.hpp
@@ -1,8 +1,31 @@
-//
-// Created by jens on 10/13/20.
-//
-
 #ifndef MASTER_VIZUALIZATION_HPP
 #define MASTER_VIZUALIZATION_HPP
+#include <ga/ga.hpp>
+
+void result_vis(GA* ga,DatasetObjectPtr datasetObjectPtr){
+    // Solution visualization
+    std::shared_ptr<GeneticEvaluatorOC> geneticEvaluatorOCPtr = std::dynamic_pointer_cast<GeneticEvaluatorOC>(ga->geneticEvaluatorPtr);
+
+    pcl::visualization::PCLVisualizer vis;
+    vis.addPointCloud(geneticEvaluatorOCPtr->pc, "data");
+    PointCloudT::Ptr mesh_pc = geneticEvaluatorOCPtr->pcm;
+
+    pcl::ExtractIndices<PointT> extractIndices;
+    for (int i = 0; i < geneticEvaluatorOCPtr->object_candidates.size(); i++) {
+        if (ga->result.best_chromosome[i]) {
+            PointCloudT::Ptr ocpc(new PointCloudT);
+            extractIndices.setInputCloud(geneticEvaluatorOCPtr->pcm);
+            extractIndices.setIndices(geneticEvaluatorOCPtr->oc_visible_pt_idxs[i]);
+            extractIndices.filter(*ocpc);
+            pcl::transformPointCloud(*ocpc, *ocpc,  geneticEvaluatorOCPtr->object_candidates[i]);
+            pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> color(ocpc, 0, 255, 0);
+            vis.addPointCloud<pcl::PointXYZ>(ocpc, color, "oc_" + std::to_string(i));
+            // Todo add camera
+        }
+    }
+
+    vis.spin();
+}
+
 
 #endif //MASTER_VIZUALIZATION_HPP
