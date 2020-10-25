3517b3e301ef5543843b10052b255536df8b32e3
jensbeltman <jens@beltman.dk>
2020-10-25 14:59:08 +0100

    Added collision checking capable of doing n^2 collision check between object candidates and returning collision pairs.

---
+++ lib/GA/include/ga/collision/collision_checking.hpp
@@ -0,0 +1,16 @@
+#ifndef MASTER_COLLISION_CHECK_HPP
+#define MASTER_COLLISION_CHECK_HPP
+
+#include <fcl/geometry/bvh/BVH_model.h>
+#include <fcl/narrowphase/collision_object.h>
+#include <ga/typedefinitions.hpp>
+#include <opencv2/viz/types.hpp>
+
+typedef fcl::BVHModel<fcl::OBBRSSd> CollisionModel;
+typedef std::shared_ptr<CollisionModel> CollisionModelPtr;
+typedef std::shared_ptr<cv::viz::Mesh> MeshPtr;
+
+std::vector<std::pair<int,int>> get_collisions(std::vector<T4> &object_candidates, MeshPtr meshPtr);
+std::ostream &operator<<(std::ostream &os, const std::vector<std::pair<int,int>>& collisions);
+
+#endif //MASTER_COLLISION_CHECK_HPP
---
+++ lib/GA/src/collision_checking.cpp
@@ -0,0 +1,89 @@
+#include "ga/collision/collision_checking.hpp"
+#include "fcl/narrowphase/collision.h"
+#include <fcl/broadphase/broadphase_dynamic_AABB_tree.h>
+#include <utility>
+
+struct CustomCollisionData {
+    fcl::CollisionRequest<double> request;
+    fcl::CollisionResult<double> result;
+    std::vector<std::pair<fcl::CollisionObject<double>*,fcl::CollisionObject<double>*>> final_results;
+
+    /// If `true`, requests that the broadphase evaluation stop.
+    bool done{false};
+};
+
+bool CustomCollisionFunction(fcl::CollisionObject<double>* o1, fcl::CollisionObject<double>* o2,
+                             void* data) {
+    assert(data != nullptr);
+    auto* collision_data = static_cast<CustomCollisionData*>(data);
+    const fcl::CollisionRequest<double>& request = collision_data->request;
+    fcl::CollisionResult<double>& result = collision_data->result;
+
+    collide(o1, o2, request, result);
+    if(result.isCollision()) { collision_data->final_results.emplace_back(o1, o2); }
+
+    result.clear();
+    return collision_data->done;
+}
+
+CollisionModelPtr get_coll_model(MeshPtr meshptr) {
+    std::vector<fcl::Vector3d> vertices;
+    std::vector<fcl::Triangle> triangles;
+    // Extract mesh data from opencv mesh type
+    for (auto itt = meshptr->cloud.begin<cv::Vec3f>(); itt != meshptr->cloud.end<cv::Vec3f>(); ++itt)
+        vertices.emplace_back((*itt)[0], (*itt)[1], (*itt)[2]);
+    for (auto itt = meshptr->polygons.begin<cv::Vec3i>(); itt != meshptr->polygons.end<cv::Vec3i>(); ++itt)
+        triangles.emplace_back((*itt)[0], (*itt)[1], (*itt)[2]);
+
+    CollisionModelPtr collModelPtr = std::make_shared<CollisionModel>();
+
+    // add the mesh data into the BVHModel structure
+    collModelPtr->beginModel();
+    collModelPtr->addSubModel(vertices, triangles);
+    collModelPtr->endModel();
+
+    return collModelPtr;
+}
+
+std::vector<std::pair<int,int>> get_collisions(std::vector<T4> &object_candidates, MeshPtr &meshPtr){
+    CollisionModelPtr collisionModelPtr = get_coll_model(meshPtr);
+
+    auto *manager = new fcl::DynamicAABBTreeCollisionManager<double>;
+    manager->setup();
+
+    // Fill collision manager with transformed collision models
+    std::vector<fcl::CollisionObject<double> *> collisionObjects;
+    for (auto & object_candidate : object_candidates) {
+        auto* collisionObject = new fcl::CollisionObject<double>(collisionModelPtr);
+        collisionObject->setTranslation(object_candidate.matrix().block<3,1>(0,3));
+        collisionObject->setRotation(object_candidate.matrix().block<3,3>(0,0));
+        collisionObjects.emplace_back(collisionObject);
+    }
+    manager->registerObjects(collisionObjects);
+
+    // Do self collision check
+    CustomCollisionData custom_collision_data;
+    manager->collide(&custom_collision_data, CustomCollisionFunction);
+
+    // Get collision pair index
+    std::vector<std::pair<int,int>> collisions;
+    int first_obj_match, second_obj_match;
+    for(auto &p:custom_collision_data.final_results){
+        first_obj_match = std::distance(collisionObjects.begin(),std::find(collisionObjects.begin(),collisionObjects.end(),p.first));
+        second_obj_match = std::distance(collisionObjects.begin(),std::find(collisionObjects.begin(),collisionObjects.end(),p.second));
+        collisions.emplace_back(first_obj_match,second_obj_match);
+    }
+
+    std::sort(collisions.begin(),collisions.end());
+
+    return collisions;
+}
+
+std::ostream &operator<<(std::ostream &os, const std::vector<std::pair<int,int>>& collisions) {
+    os << "Collisions:\n";
+    for (auto & p : collisions) {
+            os << p.first << ","<<p.second<<"\n";
+    }
+    os << "\n";
+    return os;
+};
