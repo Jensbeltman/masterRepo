be54d8859435cbffbde4d7d312faf11fbbe59156
jensbeltman <jens@beltman.dk>
2020-10-27 08:29:23 +0100

    Created simple base line based on the same oc eval that is used for GA.

---
+++ test/src/baseline.cpp
@@ -0,0 +1,60 @@
+#include <ga/genetic_evaluator/GeneticEvaluatorObjectCandidates.hpp>
+#include <ga/ga_functions.hpp>
+#include <ga/utility/visualization.hpp>
+#include <dataset/scape/ScapeDataset.hpp>
+#include <chronometer.h>
+#include <utility>
+#include <numeric>
+#include <algorithm>
+
+template<typename T>
+std::vector<size_t> sorted_idxs(std::vector<T> &vec) {
+    std::vector<size_t> idxs(vec.size());
+    std::iota(idxs.begin(), idxs.end(), 0);
+    std::stable_sort(idxs.begin(), idxs.end(), [&vec](size_t i1, size_t i2) { return vec[i1] > vec[i2]; });
+    return idxs;
+}
+
+template<typename T>
+void sort_by_idx(std::vector<size_t> &ivec, std::vector<T> &vec) {
+    std::vector<T> temp = vec;
+    for (auto &i:ivec)
+        vec[i] = temp[i];
+}
+
+int main(int argc, char** argv) {
+    int sample_n = std::stoi(argv[1]);
+    std::string object_name = argv[2];
+    Chronometer chronometer;
+    ScapeDataset scapeData("/home/jens/masterData/ScapeDataset/Scape/Full Dataset",
+                           "/home/jens/masterData/ScapeDataset/Data from Scape Recognition");
+
+
+    ScapeDatasetObjectPtr ob = std::dynamic_pointer_cast<ScapeDatasetObject>(scapeData.get_object_by_name(object_name));
+
+    auto geneticEvaluatorOCPtr = std::make_shared<GeneticEvaluatorOC>(ob, sample_n,1);
+    std::vector<T4> object_candidates = ob->get_object_candidates(sample_n);
+    std::vector<double> object_candidates_scores = ob->get_scores(sample_n);
+    std::vector<size_t> sorted_score_idx = sorted_idxs(object_candidates_scores);
+
+    chromosomeT chromosome(object_candidates.size(),false);
+    double best_cost = std::numeric_limits<double>::max();
+    double cost;
+    for (auto &i:sorted_score_idx) {
+        chromosome[i]=true;
+        cost =  geneticEvaluatorOCPtr->evaluate_chromosome(chromosome);
+        if(cost<best_cost){
+            best_cost=cost;
+        }
+        else{
+            chromosome[i]=false;
+        }
+    }
+
+    GA ga(object_candidates.size());
+    ga.geneticEvaluatorPtr = std::dynamic_pointer_cast<GeneticEvaluator>(geneticEvaluatorOCPtr);
+    ga.result.best_chromosome=chromosome;
+
+    GAResultVis vis(&ga);
+    vis.vis_result();
+}
