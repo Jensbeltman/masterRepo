36c005924a6c98a78902c10a6b8e8c47c75164d6
jensbeltman <jens@beltman.dk>
2020-11-01 20:27:04 +0100

    Now uses the new vizualiser class

--- test/src/baseline_ga_diff.cpp
+++ test/src/baseline_ga_diff.cpp
@@ -8,6 +8,7 @@
 #include <algorithm>
 #include <pcl/filters/extract_indices.h>
 #include <pcl/common/transforms.h>
+#include <ga/utility/visualization.hpp>
 
 // Baseline Function
 
@@ -25,53 +26,7 @@ void sort_by_idx(std::vector<size_t> &ivec, std::vector<T> &vec) {
     for (auto &i:ivec)
         vec[i] = temp[i];
 }
-// VIS function
-//void flip_opacity(std::vector<std::string> &ids, pcl::visualization::PCLVisualizer &vis) {
-//    if (!ids.empty()) {
-//        if (vis.contains(ids[0])){
-//            double current_opacity;
-//            vis.getPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_OPACITY, current_opacity, ids[0]);
-//            current_opacity = (current_opacity < 1.0) ? 1.0 : 0.0;
-//            for (auto &id:ids) {
-//                if (vis.contains(id)) {
-//                    vis.setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_OPACITY, current_opacity,
-//                                                         id);
-//                }
-//            }
-//        }
-//    }
-//}
-//
-//void flip_opacity(std::string id,pcl::visualization::PCLVisualizer &vis) {
-//    if (vis.contains(id)) {
-//        double current_opacity;
-//        vis.getPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_OPACITY, current_opacity, id);
-//        current_opacity = (current_opacity < 1.0) ? 1.0 : 0.0;
-//        vis.setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_OPACITY, current_opacity, id);
-//    }
-//}
-//
-//const std::string pc_id = "pc";
-//std::vector<std::string> common_accepted_ids;
-//std::vector<std::string> common_rejected_ids;
-//std::vector<std::string> baseline_rejected_ids;
-//std::vector<std::string> baseline_accepted_ids;
-//pcl::visualization::PCLVisualizer vis;
-//
-//void keyboardCallback(const pcl::visualization::KeyboardEvent &event, void *viewer_void) {
-//    if (event.keyDown()) {
-//        std::string key = event.getKeySym();
-//        if (key == "p" && event.isCtrlPressed()) {
-//            flip_opacity(pc_id);
-//        }
-//        if (key == "o" && event.isCtrlPressed()) {
-//            flip_opacity(accepted_ids);
-//        }
-//        if (key == "O" && event.isCtrlPressed() && event.isShiftPressed()) {
-//            flip_opacity(rejected_ids);
-//        }
-//    }
-//}
+
 int main(int argc, char** argv) {
     std::string object_name = argv[1];
     Chronometer chronometer;
@@ -81,83 +36,83 @@ int main(int argc, char** argv) {
 
     ScapeDatasetObjectPtr ob = std::dynamic_pointer_cast<ScapeDatasetObject>(scapeData.get_object_by_name(object_name));
     std::cout<<"Comparing baseline vs ga for object: "<<object_name<<std::endl;
+    PointCloudT::Ptr gtpc = ob->get_mesh_point_cloud();
 
-    pcl::visualization::PCLVisualizer vis;
+    CustomVisualizer vis;
     for(int sample_n = 0;sample_n<ob->size();sample_n++) {
-        std::cout<<"Sample number: "<<sample_n<<" pc file "<<ob->filenames[ob->zones[sample_n].pc_filename_idx]<<std::endl;
-        auto geneticEvaluatorOCPtr = std::make_shared<GeneticEvaluatorOC>(ob, sample_n, 1);
+        if(ob->has_gt(sample_n))
+        {
+            std::cout << "Sample number: " << sample_n << " pc file "
+                      << ob->filenames[ob->zones[sample_n].pc_filename_idx] << std::endl;
+            auto geneticEvaluatorOCPtr = std::make_shared<GeneticEvaluatorOC>(ob, sample_n, 1);
 
-        // BASELINE
-        std::vector<T4> object_candidates = ob->get_object_candidates(sample_n);
-        std::vector<double> object_candidates_scores = ob->get_scores(sample_n);
-        std::vector<size_t> sorted_score_idx = sorted_idxs(object_candidates_scores);
+            // BASELINE
+            std::vector<T4> object_candidates = ob->get_object_candidates(sample_n);
+            std::vector<T4> gts = ob->get_gt(sample_n);
+            std::vector<double> object_candidates_scores = ob->get_scores(sample_n);
+            std::vector<size_t> sorted_score_idx = sorted_idxs(object_candidates_scores);
 
-        chromosomeT bl_chromosome(object_candidates.size(), false);
-        double best_cost = std::numeric_limits<double>::max();
-        double cost;
-        for (auto &i:sorted_score_idx) {
-            bl_chromosome[i] = true;
-            cost = geneticEvaluatorOCPtr->evaluate_chromosome(bl_chromosome);
-            if (cost < best_cost) {
-                best_cost = cost;
-            } else {
-                bl_chromosome[i] = false;
+            chromosomeT bl_chromosome(object_candidates.size(), false);
+            double best_cost = std::numeric_limits<double>::max();
+            double cost;
+            for (auto &i:sorted_score_idx) {
+                bl_chromosome[i] = true;
+                cost = geneticEvaluatorOCPtr->evaluate_chromosome(bl_chromosome);
+                if (cost < best_cost) {
+                    best_cost = cost;
+                } else {
+                    bl_chromosome[i] = false;
+                }
             }
+
+            //GA
+            GA ga(object_candidates.size());
+            ga.geneticEvaluatorPtr = geneticEvaluatorOCPtr;
+            ga.N_chromosomes = 100;
+            ga.generation_max = 50;
+            ga.mutation_rate = 0.01;
+            ga.elite_count = (int) (0.1 * ga.N_genes);
+            ga.parent_pool_count = (int) (0.3 * ga.N_genes);
+            ga.crossover = crossover_uniform;
+            ga.mutation = mutation_flip;
+            GAResult result = ga.solve();
+
+
+            // VISUALISATION
+            vis.clear();
+            vis.addPointCloud(geneticEvaluatorOCPtr->pc, "Captured Point Cloud");
+            PointCloudT::Ptr mesh_pc = geneticEvaluatorOCPtr->pcm;
+            pcl::ExtractIndices<PointT> extractIndices;
+
+            for (int i = 0; i < geneticEvaluatorOCPtr->object_candidates.size(); i++) {
+                std::string id = "oc_" + std::to_string(i);
+                PointCloudT::Ptr ocpc(new PointCloudT);
+                extractIndices.setInputCloud(geneticEvaluatorOCPtr->pcm);
+                extractIndices.setIndices(geneticEvaluatorOCPtr->oc_visible_pt_idxs[i]);
+                extractIndices.filter(*ocpc);
+                pcl::transformPointCloud(*ocpc, *ocpc, geneticEvaluatorOCPtr->object_candidates[i]);
+
+                if (ga.result.best_chromosome[i] && bl_chromosome[i]) {
+                    vis.addPointCloud(ocpc, id,"Accepted by both", 0, 255, 0);
+                } else if ((!ga.result.best_chromosome[i]) && (!bl_chromosome[i])) {
+                    vis.addPointCloud(ocpc, id,"Rejected by both", 255, 0, 0);
+                } else if ((ga.result.best_chromosome[i]) && (!bl_chromosome[i])) {
+                    vis.addPointCloud(ocpc, id,"Rejcted by baseline", 255, 255, 0);
+                } else if ((!ga.result.best_chromosome[i]) && (bl_chromosome[i])) {
+                    vis.addPointCloud(ocpc, id,"Accepted by baseline", 0, 0, 255);
+                }
+            }
+
+            // Add gt to visualization
+            for (int g = 0; g < gts.size(); g++) {
+                PointCloudT::Ptr gtpc_vis = pcl::make_shared<PointCloudT>();
+                std::string id = "gt_" + std::to_string(g);
+                pcl::transformPointCloud(*gtpc, *gtpc_vis, gts[g]);
+                vis.addPointCloud(gtpc_vis, id,"Ground Truth", 0, 255, 255);
+            }
+            vis.pclVisualizer.resetCamera();
+            std::cout << "\n";
+            vis.spin();
         }
-
-        //GA
-        GA ga(object_candidates.size());
-        ga.geneticEvaluatorPtr = geneticEvaluatorOCPtr;
-        ga.N_chromosomes = 100;
-        ga.generation_max = 50;
-        ga.mutation_rate = 0.01;
-        ga.elite_count = (int) (0.1 * ga.N_genes);
-        ga.parent_pool_count = (int) (0.3 * ga.N_genes);
-        ga.crossover = crossover_uniform;
-        ga.mutation = mutation_flip;
-        GAResult result = ga.solve();
-
-
-
-        // VISUALISATION
-        vis.removeAllPointClouds();
-        vis.addPointCloud(geneticEvaluatorOCPtr->pc, "data");
-        PointCloudT::Ptr mesh_pc = geneticEvaluatorOCPtr->pcm;
-
-        pcl::ExtractIndices<PointT> extractIndices;
-        for (int i = 0; i < geneticEvaluatorOCPtr->object_candidates.size(); i++) {
-            std::string id = "oc_" + std::to_string(i);
-            PointCloudT::Ptr ocpc(new PointCloudT);
-            extractIndices.setInputCloud(geneticEvaluatorOCPtr->pcm);
-            extractIndices.setIndices(geneticEvaluatorOCPtr->oc_visible_pt_idxs[i]);
-            extractIndices.filter(*ocpc);
-            pcl::transformPointCloud(*ocpc, *ocpc, geneticEvaluatorOCPtr->object_candidates[i]);
-
-
-            int rgb[3] = {0, 0, 0};
-            if (ga.result.best_chromosome[i] && bl_chromosome[i]) {
-                rgb[1] = 255;
-//                common_accepted_ids.emplace_back(id);
-            } else if((!ga.result.best_chromosome[i]) && (!bl_chromosome[i])){
-                rgb[0] = 255;
-//                common_rejected_ids.emplace_back(id);
-            }
-            else if((ga.result.best_chromosome[i]) && (!bl_chromosome[i])){
-                rgb[0] = 255;
-                rgb[1] = 255;
-//                baseline_rejected_ids.emplace_back(id);
-            }else if((!ga.result.best_chromosome[i]) && (bl_chromosome[i])){
-                rgb[2] = 255;
-//                baseline_accepted_ids.emplace_back(id);
-            }
-            pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> color(ocpc, rgb[0], rgb[1], rgb[2]);
-            vis.addPointCloud<pcl::PointXYZ>(ocpc, color, id);
-
-            // Todo add camera
-
-        }
-        vis.resetCamera();
-        std::cout<<"\n";
-        vis.spin();
     }
 }
