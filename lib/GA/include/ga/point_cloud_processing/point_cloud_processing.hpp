30cea09375c531d54f88661226638d4abd7a09c1
jensbeltman <jens@beltman.dk>
2020-11-08 19:18:32 +0100

    Added seperate cpp file

--- lib/GA/include/ga/point_cloud_processing/point_cloud_processing.hpp
+++ lib/GA/include/ga/point_cloud_processing/point_cloud_processing.hpp
@@ -7,19 +7,7 @@
 #include "ga/typedefinitions.hpp"
 
 namespace pp {
-    pcl::IndicesPtr get_visible_indices(NormalCloudT::Ptr &nc, T4 &camera_transform) {
-        unsigned int i = 0;
-        pcl::IndicesPtr indices(new pcl::Indices);
-        Eigen::Vector3f nv;
-        Eigen::Vector3f zv = camera_transform.rotation().matrix().col(2).cast<float>();
-        for (auto &n:nc->points) {
-            nv = n.getNormalVector3fMap();
-            if (zv.dot(nv) < 0.0)
-                indices->push_back(i);
-            i++;
-        }
-
-        return indices;
-    };
+    pcl::IndicesPtr get_visible_indices(NormalCloudT::Ptr &nc, T4 &camera_transform);
+    PointCloudT::Ptr get_visible_point_cloud(PointCloudT::Ptr &pc, NormalCloudT::Ptr &nc, T4 &camera_transform);
 }
 #endif //MASTER_POINT_CLOUD_PROCESSING_HPP
