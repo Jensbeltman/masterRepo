b2390b18466dc03b3db7b9d5cf3fc85daf42bee3
jensbeltman <jens@beltman.dk>
2020-11-11 10:05:09 +0100

    Added dataset viewer

---
+++ app/dataset_viewer/main.cpp
@@ -0,0 +1,16 @@
+#include <QtWidgets/QApplication>
+#include "dataset_viewer.hpp"
+
+
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    app.setOrganizationName("University of Southern Denmark");
+    app.setOrganizationDomain("SDU");
+    app.setApplicationName("DatasetViewer");
+    QMainWindow *mainWindow = new QMainWindow;
+    DatasetViewer datasetViewer(mainWindow);
+    datasetViewer.show();
+    return app.exec();
+}
