a2fcc1b1171dd2478797f373f7cbd87e172bfed9
jensbeltman <jens@beltman.dk>
2020-12-10 14:33:33 +0100

    Uses args for folder and multiple objects. Saves results to csv. Visualization disabled for now due to crashed when closing the PCLVisualizer.

--- test/ga_and_baseline/baseline_ga_diff.cpp
+++ test/ga_and_baseline/baseline_ga_diff.cpp
@@ -12,123 +12,147 @@
 #include <pcl/common/transforms.h>
 #include <ga/visualization/point_cloud_group_visualizer.hpp>
 #include <test_util.hpp>
+#include "datautil/csv_doc.hpp"
+#include "datautil/ga_conversions.hpp"
 
 int main(int argc, char **argv) {
-    std::string object_name = argv[1];
-    bool vis_on = static_cast<bool>(std::stoi(argv[2]));
-    Chronometer chronometer;
-    ScapeDataset scapeData("/home/jens/masterData/ScapeDataset/Scape/Full_Dataset",
-                           "/home/jens/masterData/ScapeDataset/Data from Scape Recognition");
+
+    std::string s = argv[1];
+    std::string delimiter = ":";
+    std::string output_folder = argv[2];
+
+    size_t pos = 0;
+    std::string token;
+    std::vector<std::string> object_names;
+    while ((pos = s.find(delimiter)) != std::string::npos) {
+        token = s.substr(0, pos);
+        std::cout << token << std::endl;
+        object_names.push_back(token);
+        s.erase(0, pos + delimiter.length());
+    }
+    object_names.push_back(s); // Get last name
 
 
-    ScapeDatasetObjectPtr ob = std::dynamic_pointer_cast<ScapeDatasetObject>(scapeData.get_object_by_name(object_name));
-    std::cout << "Comparing baseline vs ga for object: " << object_name << std::endl;
-    PointCloudT::Ptr gtpc = ob->get_mesh_point_cloud();
-
-    PointCloudGroupVisualizer vis;
     TransformUtility tu;
-    int n_oc = 0;
-    int n_tp = 0;
-    int bl_n_tp = 0;
-    int ga_n_tp = 0;
 
-    auto geneticEvaluatorOCPtr = std::make_shared<GeneticEvaluatorOC>(ob, 0, 1);
-    GA ga(100, 100, 50, 0.05, 0.1, 0.3);
-    Baseline baseline(geneticEvaluatorOCPtr);
     // Logging
+    rapidcsv::CSVWriteDoc cost_hist_doc(output_folder+"/gaCostHist.csv",
+                                        rapidcsv::LabelParams(-1, 0));
+    rapidcsv::CSVWriteDoc results_doc(output_folder+"/results.csv", rapidcsv::LabelParams(0, 0));
+    results_doc.SetColumnName(0, "GA_Chromosome");
+    results_doc.SetColumnName(1, "BA_Chromosome");
+    results_doc.SetColumnName(2, "False_Posetives");
+    int csv_row = 0;
 
-    //auto dataObjects = json::array();
+    for(auto name:object_names) {
 
-    for (int sample_n = 0; sample_n < ob->size(); sample_n++) {
-        if (ob->has_gt(sample_n)) {
-            geneticEvaluatorOCPtr->initialise_datapoint(sample_n);
+        Chronometer chronometer;
+        ScapeDataset scapeData("/home/jens/masterData/ScapeDataset/Scape/Full_Dataset",
+                               "/home/jens/masterData/ScapeDataset/Data from Scape Recognition");
 
-            auto &dp =  geneticEvaluatorOCPtr->dp;
+        ScapeDatasetObjectPtr ob = std::dynamic_pointer_cast<ScapeDatasetObject>(
+                scapeData.get_object_by_name(name));
+        std::cout << "Comparing baseline vs ga for object: " << name << std::endl;
+        PointCloudT::Ptr gtpc = ob->get_mesh_point_cloud();
 
-            std::cout << "Sample number: " << sample_n << " pc file "
-                      << ob->scape_data_points[sample_n].pcd_filename << std::endl;
 
-            chromosomeT bl_chromosome = baseline.get_best_chromosome();
+        auto geneticEvaluatorOCPtr = std::make_shared<GeneticEvaluatorOC>(ob, 0, 1);
+        GA ga(100, 100, 50, 0.05, 0.1, 0.3);
+        ga.geneticEvaluatorPtr = std::dynamic_pointer_cast<GeneticEvaluator>(geneticEvaluatorOCPtr);
 
-            //GA
-            ga.n_genes = dp.ocs.size(); // Update chromosome size
-            ga.geneticEvaluatorPtr = geneticEvaluatorOCPtr; // Create new evaluator for the given ocs
-            GAResult result = ga.solve();
+        Baseline baseline(geneticEvaluatorOCPtr);
 
-            std::vector<bool> true_positives = tu.get_true_positives(dp.ocs, dp.gts, 10, 6);
+        for (int sample_n = 0; sample_n < ob->size(); sample_n++) {
+            if (ob->has_gt(sample_n)) {
+                geneticEvaluatorOCPtr->initialise_datapoint(sample_n);
 
-            n_oc += dp.ocs.size();
-            for (int i = 0; i < dp.ocs.size(); i++) {
-                if (true_positives[i]) {
-                    n_tp++;
-                    if (result.best_chromosome[i]) {
-                        ga_n_tp++;
+                auto &dp = geneticEvaluatorOCPtr->dp;
+
+                std::cout << "Sample number: " << sample_n << " pc file "
+                          << ob->scape_data_points[sample_n].pcd_filename << std::endl;
+
+                BAResult baResult = baseline.solve();
+
+
+                //GA
+                ga.n_genes = dp.ocs.size(); // Update chromosome size
+                GAResult gaResult = ga.solve();
+
+                std::vector<bool> false_positives = tu.get_false_positives(dp.ocs, dp.gts, 4, 0.5);
+
+
+                int n_fp = 0;
+                int bl_n_fp = 0;
+                int ga_n_fp = 0;
+
+                int n_oc = dp.ocs.size();
+                for (int i = 0; i < dp.ocs.size(); i++) {
+                    if (false_positives[i]) {
+                        n_fp++;
+                        if (gaResult.chromosome[i]) {
+                            ga_n_fp++;
+                        }
+                        if (baResult.chromosome[i]) {
+                            bl_n_fp++;
+                        }
                     }
-                    if (bl_chromosome[i]) {
-                        bl_n_tp++;
+                }
+
+                std::string row_name = ob->name_singular + "_" + std::to_string(sample_n);
+                cost_hist_doc.SetRowNameAndValue(csv_row, row_name, gaResult.cost_history);
+                results_doc.SetCell(0, csv_row, gaResult.chromosome);
+                results_doc.SetCell(1, csv_row, baResult.chromosome);
+                results_doc.SetCell(2, csv_row, false_positives);
+                results_doc.SetRowName(csv_row, row_name);
+                csv_row++;
+
+                std::cout << "Number of ocs: " << n_oc << "\tNumber of false positive: " << n_fp
+                          << "\tBaseline false positive: " << bl_n_fp
+                          << "\tGa false positive: " << ga_n_fp << std::endl;
+
+                if (false) { //vis_on || (ga_n_fp<bl_n_fp) /todo fix visualizer
+                    PointCloudGroupVisualizer vis;
+                    // VISUALISATION
+                    vis.addIdPointCloud(geneticEvaluatorOCPtr->pc, "Captured Point Cloud");
+                    PointCloudT::Ptr mesh_pc = geneticEvaluatorOCPtr->pcm;
+                    pcl::ExtractIndices<PointT> extractIndices;
+
+                    for (int i = 0; i < geneticEvaluatorOCPtr->dp.ocs.size(); i++) {
+                        std::string id = "oc_" + std::to_string(i);
+                        PointCloudT::Ptr ocpc(new PointCloudT);
+                        extractIndices.setInputCloud(geneticEvaluatorOCPtr->pcm);
+                        extractIndices.setIndices(geneticEvaluatorOCPtr->oc_visible_pt_idxs[i]);
+                        extractIndices.filter(*ocpc);
+                        pcl::transformPointCloud(*ocpc, *ocpc, geneticEvaluatorOCPtr->dp.ocs[i]);
+
+                        if (gaResult.chromosome[i] && baResult.chromosome[i]) {
+                            vis.addIdPointCloud(ocpc, id, "Accepted by both", 0, 255, 0);
+                        } else if ((!gaResult.chromosome[i]) && (!baResult.chromosome[i])) {
+                            vis.addIdPointCloud(ocpc, id, "Rejected by both", 255, 0, 0);
+                        } else if ((gaResult.chromosome[i]) && (!baResult.chromosome[i])) {
+                            vis.addIdPointCloud(ocpc, id, "Rejcted by baseline", 255, 255, 0);
+                        } else if ((!gaResult.chromosome[i]) && (baResult.chromosome[i])) {
+                            vis.addIdPointCloud(ocpc, id, "Accepted by baseline", 0, 0, 255);
+                        }
                     }
+
+                    // Add gt to visualization
+                    for (int g = 0; g < dp.gts.size(); g++) {
+                        PointCloudT::Ptr gtpc_vis = pcl::make_shared<PointCloudT>();
+                        std::string id = "gt_" + std::to_string(g);
+                        pcl::transformPointCloud(*gtpc, *gtpc_vis, dp.gts[g]);
+                        vis.addIdPointCloud(gtpc_vis, id, "Ground Truth", 0, 255, 255);
+                    }
+                    vis.resetCamera();
+                    std::cout << "\n";
+                    vis.custom_spin();
+                    std::cout << "Stopping vis" << std::endl;
+                    vis.close();
                 }
             }
-
-            std::cout << "Number of ocs: " << n_oc << "\tNumber of tps: " << n_tp << "\tBaseline tps: " << bl_n_tp
-                      << "\tGa tps: " << ga_n_tp << std::endl;
-
-            if (vis_on) {
-                // VISUALISATION
-                vis.clear();
-                vis.addIdPointCloud(geneticEvaluatorOCPtr->pc, "Captured Point Cloud");
-                PointCloudT::Ptr mesh_pc = geneticEvaluatorOCPtr->pcm;
-                pcl::ExtractIndices<PointT> extractIndices;
-
-                for (int i = 0; i < geneticEvaluatorOCPtr->object_candidates.size(); i++) {
-                    std::string id = "oc_" + std::to_string(i);
-                    PointCloudT::Ptr ocpc(new PointCloudT);
-                    extractIndices.setInputCloud(geneticEvaluatorOCPtr->pcm);
-                    extractIndices.setIndices(geneticEvaluatorOCPtr->oc_visible_pt_idxs[i]);
-                    extractIndices.filter(*ocpc);
-                    pcl::transformPointCloud(*ocpc, *ocpc, geneticEvaluatorOCPtr->object_candidates[i]);
-
-                    if (ga.result.best_chromosome[i] && bl_chromosome[i]) {
-                        vis.addIdPointCloud(ocpc, id, "Accepted by both", 0, 255, 0);
-                    } else if ((!ga.result.best_chromosome[i]) && (!bl_chromosome[i])) {
-                        vis.addIdPointCloud(ocpc, id, "Rejected by both", 255, 0, 0);
-                    } else if ((ga.result.best_chromosome[i]) && (!bl_chromosome[i])) {
-                        vis.addIdPointCloud(ocpc, id, "Rejcted by baseline", 255, 255, 0);
-                    } else if ((!ga.result.best_chromosome[i]) && (bl_chromosome[i])) {
-                        vis.addIdPointCloud(ocpc, id, "Accepted by baseline", 0, 0, 255);
-                    }
-                }
-
-                // Add gt to visualization
-                for (int g = 0; g < dp.gts.size(); g++) {
-                    PointCloudT::Ptr gtpc_vis = pcl::make_shared<PointCloudT>();
-                    std::string id = "gt_" + std::to_string(g);
-                    pcl::transformPointCloud(*gtpc, *gtpc_vis, dp.gts[g]);
-                    vis.addIdPointCloud(gtpc_vis, id, "Ground Truth", 0, 255, 255);
-                }
-                vis.resetCamera();
-                std::cout << "\n";
-                vis.custom_spin();
-            }
-
-//            dataObjects.push_back(json::object(
-//                    {
-//                            {"dataset",  scapeData.name},
-//                            {"object",   ob->name},
-//                            {"sample_n", sample_n},
-//                            {"ga",       result.best_chromosome},
-//                            {"baseline", bl_chromosome}
-//                    }
-//            ));
-
         }
     }
-//    json data_json;
-//    data_json["data"] = dataObjects;
-
-// write prettified JSON to another file
-//    std::ofstream o("../../../data/baseline_ga_comp");
-//    o << std::setw(4) << data_json << std::endl;
-//
-//    o.close();
+    cost_hist_doc.Save();
+    results_doc.Save();
+    return 0;
 }
