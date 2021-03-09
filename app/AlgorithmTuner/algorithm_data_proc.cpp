800adc4a3860e819d3502f5bbda84f683f7ad43f
jensbeltman <jens@beltman.dk>
2021-03-09 10:51:48 +0100

    Removed comments and changed the recall calculation to not use number og gt but tp+fn

--- app/AlgorithmTuner/algorithm_data_proc.cpp
+++ app/AlgorithmTuner/algorithm_data_proc.cpp
@@ -30,15 +30,12 @@ void AlgorithmDataProc::append_processed_data_to_doc(rapidcsv::CSVDocPtr &csvDoc
 
     double accuracy  = static_cast<double>( tp + tn) / static_cast<double>(tp + tn + fp + fn);
     double f1  = static_cast<double>( 2*tp) / static_cast<double>(2*tp + fp + fn);
-    double recall  = static_cast<double>(tp) / dp.gts.size();
+    double recall  = static_cast<double>(tp) /static_cast<double>(tp + fn);
     double precision  = static_cast<double>( tp) / static_cast<double>(tp + fp);
     if(std::isnan(accuracy)) accuracy = 0;
     if(std::isnan(recall)) recall = 0;
     if(std::isnan(precision)) precision = 0;
     if(std::isnan(f1)) f1 = 0;
-
-    //Write values to doc
-    //{"algName","objName","dpI","chromosome","tp","tn","fp","fn","precision","recall","accuracy","f1","time","cost","t_dist_avr","r_dist_avr","t_dist_std","r_dist_std"};
 
     csvDoc->SetCell(column_name_indices["algName"],row_i,algName);
     csvDoc->SetCell(column_name_indices["objName"],row_i,objPtr->name);
@@ -58,110 +55,7 @@ void AlgorithmDataProc::append_processed_data_to_doc(rapidcsv::CSVDocPtr &csvDoc
     csvDoc->SetCell(column_name_indices["r_dist_avr"],row_i,r_dist_avr);
     csvDoc->SetCell(column_name_indices["t_dist_std"],row_i,t_dist_std);
     csvDoc->SetCell(column_name_indices["r_dist_std"],row_i,r_dist_std);
-
-
-/*    uniqAlgNames = algName;
-    uniqObjNames = objName;
-    sort(uniqAlgNames.begin(), uniqAlgNames.end() );
-    uniqAlgNames.erase(unique(uniqAlgNames.begin(), uniqAlgNames.end() ), uniqAlgNames.end() );
-    sort(uniqObjNames.begin(), uniqObjNames.end() );
-    uniqObjNames.erase(unique(uniqObjNames.begin(), uniqObjNames.end() ), uniqObjNames.end() );*/
 }
-
-
-
-
-/*
-size_t AlgorithmDataProc::begin_index(std::string key, std::vector<std::string> &keys) {
-    return std::distance(keys.begin(),std::find(keys.begin(),keys.end(),key));
-}
-
-size_t AlgorithmDataProc::end_index(std::string key, std::vector<std::string> &keys) {
-    return std::distance(keys.begin(),std::find(keys.rbegin(),keys.rend(),key).base());
-}
-
-
-
-matplot::figure_handle  AlgorithmDataProc::bar_plot(std::string value_name) {
-   using namespace matplot;
-    auto f = figure(true);
-    auto ax = f->current_axes();
-
-    std::vector<double> valVec = derivedCSVDocPtr->GetColumn<double>(value_name);
-
-    std::vector<std::vector<double>> y;
-    for(auto &alg:uniqAlgNames){
-        y.emplace_back();
-        for(auto &obj:uniqObjNames){
-            y.back().emplace_back(get_avr(valVec,alg,obj));
-        }
-    }
-
-    auto bar_handle = ax->bar(y);
-    std::string xl = "Object Type(";
-    for(auto &alg:uniqAlgNames)
-        xl+=alg+", ";
-    xl = xl.substr(0,xl.npos-2)+")";
-    ax->xlabel(xl);
-    ax->x_axis().ticklabels(uniqObjNames);
-
-    std::vector<std::string> legends;
-    for(auto &alg:uniqAlgNames)
-        legends.emplace_back(alg + " obj avr");
-    auto legend_handle = legend(ax);
-
-    f->draw();
-    return f;
-
-}
-
-
-bool AlgorithmDataProc::get_begin_end_index(size_t &bi, size_t &ei, std::string alg_key, std::string obj_key) {
-    {
-        if(obj_key!=""){
-            bi = std::max(begin_index(alg_key, algName), begin_index(obj_key, objName));
-            ei = std::min(end_index(alg_key, algName), end_index(obj_key, objName));
-        }else{
-            bi = begin_index(alg_key, algName);
-            ei = end_index(alg_key, algName);
-        }
-        return( bi!=ei && bi < algName.size());
-    }
-}
-
-double AlgorithmDataProc::get_sum(std::vector<double> &vals, std::string alg_key, std::string obj_key) {
-        size_t bi,ei;
-        if(get_begin_end_index(bi,ei,alg_key,obj_key))
-            return std::accumulate(vals.begin()+bi,vals.begin()+ei,static_cast<double>(0));
-        else
-            return -1;
-}
-
-double AlgorithmDataProc::get_avr(std::vector<double> &vals, std::string alg_key, std::string obj_key) {
-        size_t bi,ei;
-        if(get_begin_end_index(bi,ei,alg_key,obj_key))
-            return std::accumulate(vals.begin()+bi,vals.begin()+ei,static_cast<double>(0))/static_cast<double>(ei-bi);
-        else
-            return -1;
-}
-
-int AlgorithmDataProc::get_sum(std::vector<int> &vals, std::string alg_key, std::string obj_key) {
-    size_t bi,ei;
-    if(get_begin_end_index(bi,ei,alg_key,obj_key))
-        return std::accumulate(vals.begin()+bi,vals.begin()+ei,static_cast<int>(0));
-    else
-        return -1;
-}
-
-double AlgorithmDataProc::get_avr(std::vector<int> &vals, std::string alg_key, std::string obj_key) {
-    size_t bi,ei;
-    if(get_begin_end_index(bi,ei,alg_key,obj_key))
-        return std::accumulate(vals.begin()+bi,vals.begin()+ei,static_cast<double>(0))/static_cast<double>(ei-bi);
-    else
-        return -1;
-}
-*/
-
 
 
 
--- app/AlgorithmTuner/algorithm_data_proc.hpp
+++ app/AlgorithmTuner/algorithm_data_proc.hpp
@@ -22,26 +22,6 @@ public:
     std::map<std::string,int> column_name_indices;
     void set_column_names(rapidcsv::CSVDocPtr &csvDoc);
     void append_processed_data_to_doc(rapidcsv::CSVDocPtr &csvDoc,int row_i,std::string &alg_name,DatasetObjectPtr &objPtr,int dpI, HVResult &hvResult);
-
-//    matplot::figure_handle  bar_plot(std::string value_name);
-/*
-    size_t begin_index(std::string key,std::vector<std::string> &keys);
-    size_t end_index(std::string key,std::vector<std::string> &keys);
-
-    bool get_begin_end_index(size_t &bi, size_t &ei,std::string alg_key, std::string obj_key = "");
-
-    template <typename T>
-    std::vector<double> VecIToVecD(std::vector<T> &vec){
-        return std::vector<double>(vec.begin(), vec.end());
-    }
-
-    double get_sum(std::vector<double> &vals, std::string alg_key, std::string obj_key = "");
-
-    double get_avr(std::vector<double> &vals, std::string alg_key, std::string obj_key = "");
-
-    int get_sum(std::vector<int> &vals, std::string alg_key, std::string obj_key = "");
-
-    double get_avr(std::vector<int> &vals, std::string alg_key, std::string obj_key = "");*/
 };
 
 
