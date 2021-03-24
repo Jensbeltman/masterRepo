0d6145cac5d37383833334f44b8b5b726669c710
jensbeltman <jens@beltman.dk>
2021-03-24 10:12:04 +0100

    Used new spin box utilityfunction

--- app/AlgorithmTuner/hv_interfaces.cpp
+++ app/AlgorithmTuner/hv_interfaces.cpp
@@ -20,16 +20,12 @@ void SPInterface::run(GeneticEvaluatorPtr &geneticEvaluatorPtr,HVResult &hvResul
 GAInterface::GAInterface(): HVInterface() {
     name="GA";
     // Creating Variables
-    parameters_i.emplace_back(param_i{&ga.population_size, new QSpinBox, "population_size", std::to_string(ga.population_size)});
-    parameters_i.emplace_back(param_i{&ga.generation_max, new QSpinBox, "generation_max", std::to_string(ga.generation_max)});
+    parameters_i.emplace_back(param_i{&ga.population_size, getNewSpinBox(0,10000), "population_size", std::to_string(ga.population_size)});
+    parameters_i.emplace_back(param_i{&ga.generation_max, getNewSpinBox(0,10000), "generation_max", std::to_string(ga.generation_max)});
 
     parameters_d.emplace_back(param_d{&ga.mutation_rate, new QDoubleSpinBox, "mutation_rate", std::to_string(ga.mutation_rate)});
     parameters_d.emplace_back(param_d{&ga.elite_pct, new QDoubleSpinBox, "elite_pct", std::to_string(ga.elite_pct)});
     parameters_d.emplace_back(param_d{&ga.parent_pool_pct, new QDoubleSpinBox, "parent_pool_pct", std::to_string(ga.parent_pool_pct)});
-
-    // Setting QT properties
-    for(auto & var:parameters_i)
-        var.spinBox->setMaximum(10000);
 }
 
 void GAInterface::run(GeneticEvaluatorPtr &geneticEvaluatorPtr,HVResult &hvResult) {
@@ -115,30 +111,12 @@ void RandomInterface::run(GeneticEvaluatorPtr &geneticEvaluatorPtr, HVResult &hv
 
 LogisticRegressionInterface::LogisticRegressionInterface() {
     name="LR";
-    parameters_d.emplace_back(param_d{&score_w, new QDoubleSpinBox, "score_w", std::to_string(score_w)});
-    parameters_d.back().spinBox->setMaximum(1000);
-    parameters_d.back().spinBox->setMinimum(-1000);
-    parameters_d.back().spinBox->setDecimals(4);
-    parameters_d.emplace_back(param_d{&visiblePointsFrac_w, new QDoubleSpinBox, "visiblePointsFrac_w", std::to_string(visiblePointsFrac_w)});
-    parameters_d.back().spinBox->setMaximum(1000);
-    parameters_d.back().spinBox->setMinimum(-1000);
-    parameters_d.back().spinBox->setDecimals(4);
-    parameters_d.emplace_back(param_d{&visibleInlierFrac_w, new QDoubleSpinBox, "visibleInlierFrac_w", std::to_string(visibleInlierFrac_w)});
-    parameters_d.back().spinBox->setMaximum(1000);
-    parameters_d.back().spinBox->setMinimum(-1000);
-    parameters_d.back().spinBox->setDecimals(4);
-    parameters_d.emplace_back(param_d{&penetration_w, new QDoubleSpinBox, "penetration_w", std::to_string(penetration_w)});
-    parameters_d.back().spinBox->setMaximum(1000);
-    parameters_d.back().spinBox->setMinimum(-1000);
-    parameters_d.back().spinBox->setDecimals(4);
-    parameters_d.emplace_back(param_d{&intersectingInliersFrac_w, new QDoubleSpinBox, "intersectingInliersFrac_w", std::to_string(intersectingInliersFrac_w)});
-    parameters_d.back().spinBox->setMaximum(1000);
-    parameters_d.back().spinBox->setMinimum(-1000);
-    parameters_d.back().spinBox->setDecimals(4);
-    parameters_d.emplace_back(param_d{&intecept, new QDoubleSpinBox, "intecept", std::to_string(intecept)});
-    parameters_d.back().spinBox->setMaximum(1000);
-    parameters_d.back().spinBox->setMinimum(-1000);
-    parameters_d.back().spinBox->setDecimals(4);
+    parameters_d.emplace_back(param_d{&score_w, getNewDoubleSpinBox(-1000,1000,4), "score_w", std::to_string(score_w)});
+    parameters_d.emplace_back(param_d{&visiblePointsFrac_w, getNewDoubleSpinBox(-1000,1000,4), "visiblePointsFrac_w", std::to_string(visiblePointsFrac_w)});
+    parameters_d.emplace_back(param_d{&visibleInlierFrac_w, getNewDoubleSpinBox(-1000,1000,4), "visibleInlierFrac_w", std::to_string(visibleInlierFrac_w)});
+    parameters_d.emplace_back(param_d{&penetration_w, getNewDoubleSpinBox(-1000,1000,4), "penetration_w", std::to_string(penetration_w)});
+    parameters_d.emplace_back(param_d{&intersectingInliersFrac_w, getNewDoubleSpinBox(-1000,1000,4), "intersectingInliersFrac_w", std::to_string(intersectingInliersFrac_w)});
+    parameters_d.emplace_back(param_d{&intercept, getNewDoubleSpinBox(-1000,1000,4), "intercept", std::to_string(intercept)});
 }
 
 double LogisticRegressionInterface::sigmoid(double x) {
@@ -146,8 +124,8 @@ double LogisticRegressionInterface::sigmoid(double x) {
 }
 
 void LogisticRegressionInterface::run(GeneticEvaluatorPtr &geneticEvaluatorPtr, HVResult &hvResult) {
-    if (geneticEvaluatorPtr->type == "GEUICS") {
-        auto gePtr = std::dynamic_pointer_cast<GeneticEvaluatorUniqueInlierCollisionScaled>(geneticEvaluatorPtr);
+    if (geneticEvaluatorPtr->type == "GEUICS" || geneticEvaluatorPtr->type == "GELR") {
+        auto gePtr = std::dynamic_pointer_cast<IntersectingPoints>(geneticEvaluatorPtr);
         auto &dp =  gePtr->dp;
         hvResult.chromosome.resize(dp.ocs.size());
         std::fill(hvResult.chromosome.begin(), hvResult.chromosome.end(), true);
@@ -163,12 +141,12 @@ void LogisticRegressionInterface::run(GeneticEvaluatorPtr &geneticEvaluatorPtr, 
             double visiblePoints = gePtr->visible_oc_pcs[i]->size();
             double visibleInliers = gePtr->oc_visible_inlier_pt_idxs[i]->size();
 
-            double x = scores[i]*score_w+
-                        (visiblePoints/model_points)*visiblePointsFrac_w+
-                        (visibleInliers/visiblePoints)*visibleInlierFrac_w+
-                        penetration[i]*penetration_w+
+            double x = scores[i]*score_w +
+                        (visiblePoints/model_points)*visiblePointsFrac_w +
+                        (visibleInliers/visiblePoints)*visibleInlierFrac_w +
+                        penetration[i]*penetration_w +
                         (intersections[i]/visibleInliers)*intersectingInliersFrac_w
-                        +intecept;
+                       + intercept;
             x = sigmoid(x);
             hvResult.chromosome[i] = (x >= sigmoid_cutoff);
         }
